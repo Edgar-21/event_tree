8121b778e7eeb44845da6474e4b6aa864f7cc65b
Edgar-21 <ejpflug@gmail.com>
2025-04-16 12:38:42 -0500

    functionally perfect i think

--- event_tree.py
+++ event_tree.py
@@ -12,6 +12,7 @@ class Node(object):
         self.x = 0
         self.y = 0
         self.y_jog = 0
+        self.terminal_node = False
 
     @property
     def parent_succeeded(self):
@@ -23,18 +24,8 @@ class Node(object):
             self._parent_succeeded = False
         return self._parent_succeeded
 
-    def determine_child_splits(self):
-        terminations = 0
-        # print("im a node")
-        # print(self.label)
-        # print(self.child_nodes)
-        for node in self.child_nodes:
-            terminations += node.determine_child_splits()
-        if not self.child_nodes:
-            terminations += 2
-        return terminations
-
     def print_children(self):
+        print(self)
         for node in self.child_nodes:
             node.print_children()
 
@@ -45,18 +36,22 @@ class Node(object):
             end_states += node.count_child_end_states()
         if len(self.child_nodes) == 0:
             end_states += 1
+        if end_states == 0:
+            if self.split_fraction != 0:
+                end_states = 1
         return end_states
 
     def determine_vertical_jog(self, y_step):
         end_states = 0
+
+        self.y_jog = y_step
 
         # if its the end of the line it doesnt matter
         for node in self.child_nodes:
             node.determine_vertical_jog(y_step)
             if node.parent_succeeded:
                 end_states = node.count_child_end_states()
-
-        self.y_jog = (end_states) * y_step
+                self.y_jog = end_states * y_step
 
     def set_child_locations(self, x_jog):
         for child_node in self.child_nodes:
@@ -71,13 +66,14 @@ class Node(object):
     def plot_node(self, fig):
         ax = fig.gca()  # Get the current axes from the figure
         ax.plot(self.x, self.y, "o")  # Plot the point using the axes
-        ax.annotate(
-            f"P({self.label})={self.split_fraction:.4e}",
-            (self.x, self.y + self.y_jog),
-            textcoords="offset points",
-            xytext=(5, 5),
-            ha="left",
-        )  # Add label next to the point
+        if self.split_fraction != 0:
+            ax.annotate(
+                f"P({self.label})={self.split_fraction:.4e}",
+                (self.x, self.y + self.y_jog),
+                textcoords="offset points",
+                xytext=(5, 5),
+                ha="left",
+            )  # Add label next to the point
 
     def plot_node_and_children(self, fig):
         self.plot_node(fig)
@@ -93,24 +89,73 @@ class Node(object):
             else:
                 ax.plot([self.x, self.x], [self.y, node.y])
                 ax.plot([self.x, node.x], [node.y, node.y])
-        pass
 
-    def add_end_states(self):
+    def add_end_states(self, top_events):
         for node in self.child_nodes:
-            node.add_end_states()
-        if "|" in self.label:
+            node.add_end_states(top_events)
+        if len(self.child_nodes) == 0 and "|" in self.label:
             event, preceeding_sequence = self.label.split("|")
-        if len(self.child_nodes) == 0:
-            if self.split_fraction != 0:
+            last_node = self
+            if event != top_events[-1]:
+                for next_event in top_events[top_events.index(event) + 1 :]:
+                    preceeding_sequence = event + "'/" + preceeding_sequence
+                    label = next_event + "|" + preceeding_sequence
+                    event = next_event
+                    new_node = Node(label, last_node, 0)
+                    last_node.child_nodes.add(new_node)
+                    last_node = new_node
+                pass
+            if last_node.split_fraction != 0:
                 label = event + "'/" + preceeding_sequence
-                success_node = Node(label, self)
+                success_node = Node(label, last_node)
                 label = event + "/" + preceeding_sequence
                 failure_node = Node(label, self)
-                self.child_nodes = [success_node, failure_node]
+                last_node.child_nodes = set({success_node, failure_node})
             else:
                 label = event + "'/" + preceeding_sequence
-                success_node = Node(label, self)
-                self.child_nodes = [success_node]
+                success_node = Node(label, last_node)
+                last_node.child_nodes = set({success_node})
+
+    def compute_node_probability(self):
+        chance_of_occurance = 1
+        # ask my parent what their split fraction leading to me was
+        if self.parent_node is not None:
+            if self.parent_succeeded:
+                chance_of_occurance = 1 - self.parent_node.split_fraction
+            else:
+                chance_of_occurance = self.parent_node.split_fraction
+            # parent asks their parent
+            chance_of_occurance *= self.parent_node.compute_node_probability()
+
+        return chance_of_occurance
+
+    def label_end_nodes(self, fig):
+        for node in self.child_nodes:
+            node.label_end_nodes(fig)
+        if len(self.child_nodes) == 0:
+            probability = self.compute_node_probability()
+            ax = fig.gca()
+            ax.annotate(
+                f"{self.label}: {probability:0.4e}",
+                (self.x, self.y),
+                textcoords="offset points",
+                xytext=(5, 5),
+                ha="left",
+            )
+
+    def grow_tree(self, top_events):
+        for node in self.child_nodes:
+            node.grow_tree(top_events)
+            node.bud_node(top_events)
+
+    def bud_node(self, top_events):
+        if self.split_fraction != 0:
+            if len(self.child_nodes) == 1:
+                event, preceeding_sequence = self.label.split("|")
+                next_event = top_events[top_events.index(event) + 1]
+                label = f"{next_event}|{event}'/{preceeding_sequence}"
+                new_node = Node(label, self, 0)
+                self.child_nodes.add(new_node)
 
     def __repr__(self):
         return self.label
@@ -121,7 +166,9 @@ class NodeBasedTree(object):
     def __init__(self, initiating_frequency, top_events):
         self.top_events = top_events.split("/")
         self.initiating_event = self.top_events[0]
-        self.parent_node = Node(self.initiating_event)
+        self.parent_node = Node(
+            self.initiating_event, split_fraction=initiating_frequency
+        )
         self.nodes = {self.initiating_event: self.parent_node}
         self.add_top_sequence()
 
@@ -144,13 +191,10 @@ class NodeBasedTree(object):
 
         events_list = sequence.split("/")
         events = dict(zip(events_list, split_fractions))
-        print(sequence)
-        print(events)
         last_node = self.parent_node
         label_str = self.initiating_event + "/"
 
         for top_event in self.top_events[1:]:
-            print(top_event)
             label = top_event + "|" + label_str.rstrip("/")
             active_node = self.nodes.get(
                 label, Node(label, last_node, split_fraction=0)
@@ -168,50 +212,73 @@ class NodeBasedTree(object):
             last_node.child_nodes.add(active_node)
             last_node = active_node
 
+    def label_top_events(self, fig):
+        x_loc = 0
+        ax = fig.gca()
+        for event in self.top_events:
+            ax.annotate(
+                f"{event}",
+                (x_loc, -self.y_step / 2),
+                textcoords="offset points",
+                xytext=(5, 5),
+                ha="center",
+            )
+            x_loc += self.x_step
+
+        ax.annotate(
+            f"END STATE",
+            (x_loc, -self.y_step / 2),
+            textcoords="offset points",
+            xytext=(5, 5),
+            ha="left",
+        )
+
     def plot_tree(self, y_step=-1, x_step=1):
+        self.y_step = y_step
+        self.x_step = x_step
         fig = plt.figure()
-        self.parent_node.add_end_states()
+        self.parent_node.grow_tree(self.top_events)
+        self.parent_node.add_end_states(self.top_events)
+
         self.parent_node.determine_vertical_jog(y_step)
         self.parent_node.set_child_locations(x_step)
         self.parent_node.plot_node_and_children(fig)
         self.parent_node.connect_with_children(fig)
+        self.parent_node.label_end_nodes(fig)
+        self.label_top_events(fig)
 
         ax = fig.gca()
         xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        ax.set_ylim([ylim[0], -y_step])
         ylim = ax.get_ylim()
 
         x_range = xlim[1] - xlim[0]
         y_range = ylim[1] - ylim[0]
 
         fig.set_size_inches(abs(x_range), abs(y_range))
-
+        plt.tight_layout()
         return fig
 
 
 def test_plots(tree):
     fig = tree.plot_tree(y_step=-1, x_step=4)
 
-    plt.tight_layout()
     plt.axis("off")
     plt.savefig("connect.png", bbox_inches="tight")
 
+    return tree
+
 
 def main():
-    top_events = "i/A/B/C"
-    sequences = {
-        "A": [0.1],
-        "B/C": [0.1, 0.01],
-        "A/B/C": [0.1, 0.2, 0.2],
-        "C": [0.5],
-    }
+    top_events = "i/A/B/C/D"
+    sequences = {"B/C": [0.1, 0.01], "A/B/C/D": [0.1, 0.2, 0.3, 0.4]}
     tree = NodeBasedTree(0.08, top_events)
     for sequence, split_fractions in sequences.items():
         tree.add_sequence(sequence, split_fractions)
-    test_plots(tree)
-
-    tn = tree.nodes["B|A'/i"]
-    print(tn.split_fraction)
-    print(tn.parent_node)
+    tree = test_plots(tree)
+    # tree.plot_tree()
+    # tree.parent_node.print_children()
 
 
 if __name__ == "__main__":
